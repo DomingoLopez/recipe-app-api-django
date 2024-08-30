cff5f2ef1548ddb74012210d0cd1df1034cc3da1
Domingo López <domin68914@gmail.com>
2024-08-30 14:39:57 +0200

    fixing linting

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,11 +2,9 @@
 Calculator functions
 """
 
-
 def add(x,y):
     """Add x and y and return result."""
     return x + y
-
 
 def substract(x,y):
     """Substract x from y and return result"""
--- app/app/tests.py
+++ app/app/tests.py
@@ -13,16 +13,15 @@ class CalcTests(SimpleTestCase):
     Args:
         SimpleTestCase (_type_): _description_
     """
-    
     def test_add_numbers(self):
         """Test adding numbers together"""
-        res = calc.add(5,6)
+        res = calc.add(5, 6)
         
-        self.assertEqual(res,11)
+        self.assertEqual(res, 11)
         
-    
     def test_substract_numbers(self):
         """Test substracting numbers"""
-        res = calc.substract(10,15)
+        res = calc.substract(10, 15)
         
-        self.assertEqual(res,-5)
+        self.assertEqual(res, -5)
+        
