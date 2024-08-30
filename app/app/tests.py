6cf81aec4734056bc677cb0d9cd449b3c711a7ff
Domingo López <domin68914@gmail.com>
2024-08-30 14:45:14 +0200

    fixing linting v2

--- app/app/tests.py
+++ app/app/tests.py
@@ -7,21 +7,18 @@ from django.test import SimpleTestCase
 # Importación de módulo a testar
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
     """Test the calc module
-
     Args:
         SimpleTestCase (_type_): _description_
     """
     def test_add_numbers(self):
         """Test adding numbers together"""
         res = calc.add(5, 6)
-        
         self.assertEqual(res, 11)
-        
+
     def test_substract_numbers(self):
         """Test substracting numbers"""
         res = calc.substract(10, 15)
-        
         self.assertEqual(res, -5)
-        
