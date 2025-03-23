57629e698a1705baaef49ca6d011de358ab649f9
Domingo Lopez <domin68914@gmail.com>
2025-03-23 19:53:45 +0100

    added testing db connection plus tests

---
+++ app/core/tests/test_commands.py
@@ -0,0 +1,42 @@
+"""
+Test custom Django management commands.
+"""
+
+from unittest.mock import patch
+
+from psycopg2 import OperationalError as Psycopg2Error
+
+from django.core.management import call_command
+from django.db.utils import OperationalError
+from django.test import SimpleTestCase
+
+
+@patch('core.management.commands.wait_for_db.Command.check')
+class CommandTests(SimpleTestCase):
+    """Test commands."""
+
+    def test_wait_for_db_ready(self, patched_check):
+        """Test waiting for database if database ready."""
+        patched_check.return_value = True
+
+        call_command('wait_for_db')
+
+        patched_check.assert_called_once_with(databases=['default'])
+
+    # Ojito el orden en el que se ponen los patch y el orden
+    # que se le pasa a la función.
+    # Siempre de dentro hacia fuera, primero a nivel función
+    # luego nivel clase
+    @patch('time.sleep')
+    def test_wait_for_db_delay(self, patched_sleep, patched_check):
+        """Test waiting for database when getting OperationalError."""
+        # Esto es para que las primeras dos veces lance un psycopg2Error,
+        # las 3 siguientes un operationalError y al final True
+        # Y se hace así con el patch
+        patched_check.side_effect = [Psycopg2Error] * 2 + \
+            [OperationalError] * 3 + [True]
+
+        call_command('wait_for_db')
+
+        self.assertEqual(patched_check.call_count, 6)
+        patched_check.assert_called_with(databases=['default'])
